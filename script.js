71c4582499c6b5c50e01038dddbb366d54121121
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-03-22 22:14:11 -0300

    Added basic painting behavior

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let container = document.querySelector("#container");
 let totalSquares = 256;
 let squaresPerRow = Math.sqrt(totalSquares);
+let colorSelector = document.querySelector("#color");
+let mouseDown = false;
+
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 for (let i = 0; i < totalSquares; i++) {
   let squareDiv = document.createElement("div");
   squareDiv.className = "square";
-  squareDiv.appendChild(document.createTextNode("\u00A0"));
   squareDiv.style.minWidth = 100 / squaresPerRow + "%";
   squareDiv.style.height = 0;
   squareDiv.style.paddingBottom = squareDiv.style.minWidth;
@@ -14,8 +18,10 @@ for (let i = 0; i < totalSquares; i++) {
 
 let allSquares = document.querySelectorAll(".square");
 allSquares.forEach((square) => {
-  square.addEventListener("mouseenter", function () {
-    square.style.backgroundColor = "red";
+  square.addEventListener("mousemove", function () {
+    if (mouseDown) {
+      square.style.backgroundColor = colorSelector.value;
+    }
   });
 });
 
